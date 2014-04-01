44ee58c028f39a32dffce21b95a63c1fecb86073
mrlong <mrlong.com@gmail.com>
2014-04-01 12:53:53 +0800

    修改:   1.增加显示最新的五个书法作品

--- mrlongapp.js
+++ mrlongapp.js
@@ -16,15 +16,27 @@ app.use(connect.static(__dirname + '/public', { maxAge: 86400000 }));
 var tpl = ejs.compile(fs.readFileSync(path.join(__dirname, 'views/index.html'),'utf-8'));
 app.use(function (req, res,next) {	
   var data=[];
-	var filenames = fs.readdirSync(__dirname + '/public/shf');  
-	for (i = 0; i < filenames.length; i++) {  
-    if(filenames[i].indexOf('.JPG')>0){
-    	data.push(filenames[i]);
-    	if(data.length>5){break;};
-    };
-	};  
-  res.writeHead(200);
-  res.end(tpl({'imgs':data}));  	 
+	//var filenames = fs.readdirSync(__dirname + '/public/shf');
+  //对文件进行排序
+  fs.readdir(__dirname + '/public/shf', function(err, filenames){
+    filenames.sort(function(val1, val2){
+      //读取文件信息
+      var stat1 = fs.statSync(__dirname + '/public/shf/' + val1);
+      var stat2 = fs.statSync(__dirname + '/public/shf/' + val2);
+      //根据时间从最新到最旧排序
+      return stat2.mtime - stat1.mtime;
+    });
+
+    for (i = 0; i < filenames.length; i++) {  
+      if((filenames[i].indexOf('.jpg')>0) || (filenames[i].indexOf('.JPG')>0)) {
+    	   data.push(filenames[i]);
+    	   if(data.length>5){break;};
+      };
+	   };  
+    res.writeHead(200);
+    //console.log(data);
+    res.end(tpl({'imgs':data}));  	 
+  });
 });
 
 
