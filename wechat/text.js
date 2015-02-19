e26bae1a82d0f5ed5436b87f3cc628c63bcdf8b8
龙仕云 <mrlong.com@gmail.com>
2015-02-19 09:59:16 +0800

    修改：   还原原来的text.js
    Signed-off-by: 龙仕云 <mrlong.com@gmail.com>

---
+++ wechat/text.js
@@ -0,0 +1,31 @@
+//
+// 作者：龙仕云  2014-3-18
+// 文本信息推送器 
+//
+
+var config = require('../config');
+
+module.exports = function(message, req, res, next){
+  //console.log(message);
+  
+  var input = (message.Content || '').trim();
+
+  var content = '';
+  content.push({title:'你要选择:'});
+  
+  
+  content.push({
+    title: '读书笔记',
+    type: 'text',
+    url: config.domain + '/books/notes/add?txt=' + input
+  });
+  
+  content.push({
+    title: '格言',
+    url: config.domain + '/motto/add?txt=' + input
+  });
+  
+  
+  res.reply(content);
+  
+};
