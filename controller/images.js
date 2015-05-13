98289395c8ebc7fa66190ba339a48373a390a5d8
龙仕云 <mrlong.com@gmail.com>
2015-05-13 17:06:21 +0800

    sss
    Signed-off-by: 龙仕云 <mrlong.com@gmail.com>

--- controller/images.js
+++ controller/images.js
@@ -6,6 +6,9 @@
 var express = require('express');
 var db = require('../db');
 var markdown = require('markdown').markdown;
+var path = require('path');
+var config = require('../config');
+var fs = require('fs');
 
 
 var router = express.Router();
