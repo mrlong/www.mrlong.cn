a2729c1605df191df52c80ffdcf8225590c96efe
龙仕云 <mrlong.com@gmail.com>
2019-04-06 21:34:12 +0800

    test

--- wechat/text.js
+++ wechat/text.js
@@ -12,52 +12,31 @@ module.exports = function(message, req, res, next){
   
   var input = (message.Content || '').trim();
 
-  var content = [];
+  
+  var content = '';
 
-  content.push({
-    title:'选择你要做什么？',
-    description: '这是女神与高富帅之间的对话',
-    picurl:'http://www.mrlong.cn/img/selfwechat.png',
-    url:config.domain});
+  //content.push({
+  //  title:'选择你要做什么？',
+  //  url:config.domain});
   
   //是数字
   if(!isNaN(input)){
-    
-    content.push({
-      title: '1、我的花销',
-      description: '这是女神与高富帅之间的对话',
-      picurl:'http://www.mrlong.cn/img/selfwechat.png',
-      url: config.domain + '/cost/add?txt=' + input
-    });
-    
-    res.reply('1、我的花销 ' + config.domain + '/cost/add?txt=' + input);
-    //  content: 'text object',
-    //  type:
-    //});
+  
+    //res.reply('1、我的花销 ' + config.domain + '/cost/add?txt=' + input);
     //console.log(content);
-    //res.reply(content);
+    res.reply([{
+      title: '1、我的花销',
+      url: config.domain + '/cost/add?txt=' + input
+    }]);
   }
   else{
   
-    content.push({
-      title: '1、读书笔记',
-      url: config.domain + '/books/notes/add?txt=' + input
-    });
-  
-    content.push({
-      title: '2、格言',
-      url: config.domain + '/motto/we_add?txt=' + input
-    });
-  
-    content.push({
-      title: '3、增加人脉',
-      url: config.domain + '/friend/add?txt=' + input
-    });
+
+    content = content + ' ' +  '1、读书笔记 ' + config.domain + '/books/notes/add?txt=' + input ;    
+    content = content + ' ' +  '2、格言 ' + config.domain + '/motto/we_add?txt=' + input;
+    content = content + ' ' +  '3、增加人脉 ' + config.domain + '/friend/add?txt=' + input;
+    content = content + ' ' +  '4、事件提醒 ' + config.domain + '/remind/add?txt=' + input;
     
-    content.push({
-      title: '4、事件提醒',
-      url: config.domain + '/remind/add?txt=' + input
-    });
     
     //输入的日期格式说明可能是提取misfit内容
     if(input.length>2 && input.indexOf('-')>=0){
@@ -72,37 +51,25 @@ module.exports = function(message, req, res, next){
       var url = 'https://api.misfitwearables.com/auth/dialog/authorize?response_type=code&client_id=pYPABsuKSXpOByXr&redirect_uri=' +
                 redirectUri + '&scope=public,birthday,email,tracking,session,sleeps';
       
-      content.push({
-        title: '5、提取misfit数据',
-        url: url 
-      });
+      content = content + ' ' +  '5、提取misfit数据 ' + url;
       res.reply(content);
     }
     
     //直接显示姓名电话会直难过点。但速度会慢
     else if (input.length ==3 || input.length ==2){
       db.query('select fri_name,fri_guid,fri_moblie from friend where fri_name=?',[input],function(err,rows){
-        if(!err && rows.length>0){
-          content.push({
-            title: '5、我要找人 ' + rows[0].fri_name + ' ' + rows[0].fri_moblie,
-            url: config.domain + '/friend/search?txt=' + input
-          });      
+        if(!err && rows.length>0){    
+          content = content + ' ' +  '5、我要找人 ' + rows[0].fri_name + ' ' + rows[0].fri_moblie + ' ' + config.domain + '/friend/search?txt=' + input; 
         }
         else{
-          content.push({
-            title: '5、我要找人',
-            url: config.domain + '/friend/search?txt=' + input
-          }); 
+          content = content + ' ' +  '5、我要找人 ' +   config.domain + '/friend/search?txt=' + input; 
         };
         res.reply(content);  //因为异步，所以将移到这地方
       });
     
     }
     else{
-      content.push({
-          title: '5、我要找人',
-          url: config.domain + '/friend/search?txt=' + input
-      }); 
+      content = content + ' ' +  '5、我要找人 ' +   config.domain + '/friend/search?txt=' + input; 
       res.reply(content);
     };
     //end
